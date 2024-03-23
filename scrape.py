e249923d9cd2b6f2f981c3936a5ff8d515101fa1
ParthSareen <psareen@uwaterloo.ca>
2024-03-23 13:32:31 -0400

    parth push

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 
 # The base URL of the website to scrape
 # base_url = "http://reports.ieso.ca/public/RealtimeConstTotals/"
@@ -14,22 +15,27 @@ response.raise_for_status()  # Raise an HTTPError if the HTTP request returned a
 # Parse the HTML content of the page with BeautifulSoup
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Find all the links on the page that end with '.csv'
-csv_links = [link.get('href') for link in soup.find_all('a') if link.get('href').endswith('.csv')]
+# Define a regular expression pattern to match CSV files from 2023 onwards
+pattern = re.compile(r'.*_(2023|202[4-9]|20[3-9]\d|2[1-9]\d{2})\d{4}\.csv')
+pattern_2023 = re.compile(r'.*_(2023)\d{4}\.csv')
+pattern_2024 = re.compile(r'.*_(2024)\d{4}\.csv')
+# Find all the links on the page that match the pattern for 2023 or above
+csv_links = [link.get('href') for link in soup.find_all('a', href=pattern)]
 
 # Create a directory to store the downloaded files
-os.makedirs('data/weekly_market', exist_ok=True)
+os.makedirs('data/weekly_market2', exist_ok=True)
 
-# Download each CSV file
+# Download each CSV file that matches the pattern
 for link in csv_links:
+
     csv_url = base_url + link
     csv_response = requests.get(csv_url)
     csv_response.raise_for_status()
 
     # Write the content of the CSV to a file
-    filename = os.path.join('data/weekly_market', link.split('/')[-1])
+    filename = os.path.join('data/weekly_market2', link.split('/')[-1])
     with open(filename, 'wb') as file:
         file.write(csv_response.content)
     print(f'Downloaded {filename}')
 
-print('All CSV files have been downloaded.')
+print('All CSV files from 2023 and above have been downloaded.')
